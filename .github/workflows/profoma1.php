86760e6c9585900d2cd13f0e4f1219fa1dab0ba9
johar07 <joharsayyed4@gmail.com>
2023-03-24 12:39:50 +0530

    this fixes #1

---
+++ .github/workflows/profoma1.php
@@ -0,0 +1,325 @@
+<?php
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="main.css">
+
+    <title>Proforma No.1.</title>
+  </head>
+  <body>
+    <div class="heading">
+    <h1>   Profoma to be submitted for to establish </h1>
+            <h2>   P.A Inamdar Computer  </h2>
+                <h3>  Proforma No.1. </h3>
+    </div>
+    <div class="container">
+    <div>
+        <form action="index.php">
+            <table class="forum">
+                    <tr>
+                        <td><br>
+                            Name of the Educational Trust:
+                            <br>
+                            <br>
+                    
+                        </td>
+                        <td>
+                            <br>
+                            <input type="text" placeholder="Name of the Educational Trust" name="pf1educationalname"required size="60">
+                            <br>
+                            <br>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Full Address:
+                            <br>
+                            <br>
+                    
+                        </td>
+                        <td>
+                            <input type="text" placeholder="Full Address" name="pf1address" required size="60">
+                            <br>
+                            <br>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Contact No:
+                            <br>
+                            <br>
+                        </td>
+                        <td>
+                            <input type="phone" placeholder="Contact No" name="pf1contactno"required size="11" maxlength="10">
+                            <br>
+                            <br>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Mobile No:
+                            <br>
+                            <br>
+                        </td>
+                        <td>
+                            <input type="phone" placeholder="Mobile No" name="pf1moblieno"required size="11" maxlength="10">
+                            <br>
+                            <br>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Email Id:
+                            <br>
+                            <br>
+                        </td>
+                        <td>
+                            <input type="email" placeholder="Email Id" name="emailid" required size="50">
+                            <br>
+                            <br>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Name of president/Chairman:
+                            <br>
+                            <br>
+                        </td>
+                        <td>
+                            <input type="text" placeholder="Name of president/Chairman" name="presidentname" required size="50">
+                            <br>
+                            <br>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Contact No:
+                            <br>
+                            <br>
+                        </td>
+                        <td>
+                            <input type="phone" placeholder="Contact No" name="presidentcontactno" required size="11" maxlength="10">
+                            <br>
+                            <br>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Mobile No:
+                            <br>
+                            <br>
+                        </td>
+                        <td>
+                            <input type="phone" placeholder="Mobile No" name="presidentmobileno" required  size="11" maxlength="10">
+                            <br>
+                            <br>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Emali Id:
+                            <br>
+                            <br>
+                        </td>
+                        <td>
+                            <input type="email" placeholder="Emali Id" name="presidentemail" required size="50">
+                            <br>
+                            <br>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Name of the Hon.Secretary:
+                            <br>
+                            <br>
+                        </td>
+                        <td>
+                            <input type="text" placeholder="Name of the Hon.Secretary" name="secretaryname" required size="50">
+                            <br>
+                            <br>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Contact No:
+                            <br>
+                            <br>
+                        </td>
+                        <td>
+                            <input type="phone"  placeholder="Contact No" name="secretary" required size="11" maxlength="10">
+                            <br>
+                            <br>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Mobile No:
+                            <br>
+                            <br>
+                        </td>
+                        <td>
+                            <input type="phone"  placeholder="Mobile No" name="secretary" required size="11" maxlength="10">
+                            <br>
+                            <br>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Emali Id:
+                            <br>
+                            <br>
+                        </td>
+                        <td>
+                            <input type="email"  placeholder="Email Id" name="secretary" required size="50">
+                            <br>
+                            <br>
+                        </td>
+                    </tr><tr>
+                        <td>
+                            Name of the Coordinator Wiht PAI-ICT Academy Pune:
+                            <br>
+                            <br>
+                        </td>
+                        <td>
+                            <input type="text"  placeholder="Name of the Coordinator Wiht PAI-ICT Academy Pune" name="paiict" required size="50">
+                            <br>
+                            <br>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Contact No:
+                            <br>
+                            <br>
+                        </td>
+                        <td>
+                            <input type="phone" placeholder="Contact No" name="paiictcontact" required size="11" maxlength="10">
+                            <br>
+                            <br>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Mobile No:
+                            <br>
+                            <br>
+                        </td>
+                        <td>
+                            <input type="phone" placeholder="Mobile No" name="paiictmobileno" required size="11" maxlength="10">
+                            <br>
+                            <br>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Emali Id:
+                            <br>
+                            <br>
+                        </td>
+                        <td>
+                            <input type="email" placeholder="Email Id" name="paiictemailid" required size="50">
+                            <br>
+                            <br>
+                        </td>
+                    </tr>
+                    <!-- <tr>
+                        <td>
+                    
+                    <div id="dateandsign"  class="container-fluid" style="display: flex;">
+                     <div style="flex: 4;">
+                        <label for="pf1dated">Dated:</label>
+                        <input type="date" id="pf1dated" name="pf1dated">
+                     </div>
+                    </td>
+                     <td>
+                        <div style="flex: -2;">
+                            <p style="font-weight: bold;">Signature ..................</p>
+                        </div>
+                    </div>
+                    </td>
+                    </tr> -->
+
+
+            </table>
+<!-- date and sign  -->
+            <div id="dateandsign"  class="container-fluid" style="display: flex;">
+                     <div style="flex: 4;">
+                        <label for="pf1dated">Dated:</label>
+                        <input type="date" id="pf1dated" name="pf1dated">
+                     </div>
+                   
+                        <div style="flex: -2;">
+                            <p style="font-weight: bold;">Signature ..................</p>
+                        </div>
+                    </div>
+<!-- date and sign ends here -->
+<br>
+
+<!-- submit and reset -->
+            <div class="srbutton">
+                    <input type="submit" class="btn btn-primary" value="submit"  >
+                    <input type="reset" class="btn btn-primary" value="reset"  >
+            </div>
+
+
+        </form>    
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+    
+    </div>
+
+
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
