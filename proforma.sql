1c1dd596e0c9459853704e1a5832bfa18de24329
Saikishor Rasala <rasalasaikishor@gmail.com>
2023-04-27 22:33:57 +0530

    update Proforma.sql

--- proforma.sql
+++ proforma.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 30, 2023 at 12:57 PM
+-- Generation Time: Apr 27, 2023 at 07:03 PM
 -- Server version: 10.4.27-MariaDB
--- PHP Version: 8.0.25
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,44 +24,70 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `form1`
+-- Table structure for table `pofoma2`
 --
 
-CREATE TABLE `form1` (
-  `form1ID` int(11) NOT NULL,
-  `educationalname` text NOT NULL,
-  `educationaladdress` varchar(70) NOT NULL,
-  `location` varchar(70) NOT NULL,
-  `educationalcontact` int(10) NOT NULL,
-  `educationalmobile` int(10) NOT NULL,
-  `educationalemail` varchar(30) NOT NULL,
-  `presidentname` text NOT NULL,
-  `presidentcontact` int(10) NOT NULL,
-  `Presidentmobile` int(10) NOT NULL,
-  `presidentemail` varchar(30) NOT NULL,
-  `secretaryname` text NOT NULL,
-  `secretarycontact` int(10) NOT NULL,
-  `secretarymobile` int(10) NOT NULL,
-  `secretaryemail` varchar(30) NOT NULL,
-  `coordinatorname` text NOT NULL,
-  `coordinatorcontact` int(10) NOT NULL,
-  `coordinatormobile` int(10) NOT NULL,
-  `coordinatoremail` varchar(30) NOT NULL,
-  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+CREATE TABLE `pofoma2` (
+  `schooljr` text NOT NULL,
+  `school` enum('primary','high','jr') NOT NULL,
+  `Instructionname` text NOT NULL,
+  `primaryname` text NOT NULL,
+  `adid` enum('aided','unaided') NOT NULL,
+  `strength` text NOT NULL,
+  `teachingstaff` int(11) NOT NULL,
+  `secondaryschool` int(11) NOT NULL,
+  `adid2` enum('aided','unaided') NOT NULL,
+  `secstrength` int(11) NOT NULL,
+  `secteachingstaffsec` int(11) NOT NULL,
+  `jrschool` int(11) NOT NULL,
+  `adid3` enum('aided','unaided') NOT NULL,
+  `jrstrength` int(11) NOT NULL,
+  `jrteachingstaff` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Indexes for dumped tables
+-- Dumping data for table `pofoma2`
 --
 
+INSERT INTO `pofoma2` (`schooljr`, `school`, `Instructionname`, `primaryname`, `adid`, `strength`, `teachingstaff`, `secondaryschool`, `adid2`, `secstrength`, `secteachingstaffsec`, `jrschool`, `adid3`, `jrstrength`, `jrteachingstaff`, `date`) VALUES
+('dfgh', '', 'dsgfhm', 'dfgh', '', ' web devloper resume saikishor 1.pdf', 0, 0, '', 0, 0, 0, '', 0, 0, '2023-04-26 18:30:00'),
+('sfdfdvc', '', 'wefrgthdymu,io/[', 'zsdsvfgxnvbk.j', '', ' web devloper resume saikishor 1.pdf', 0, 0, '', 0, 0, 0, '', 0, 0, '2023-04-26 18:30:00'),
+('sfdfdvc', '', 'wefrgthdymu,io/[', 'zsdsvfgxnvbk.j', '', ' web devloper resume saikishor 1.pdf', 0, 0, '', 0, 0, 0, '', 0, 0, '2023-04-26 18:30:00'),
+('sfdfdvc', '', 'wefrgthdymu,io/[', 'zsdsvfgxnvbk.j', '', ' web devloper resume saikishor 1.pdf', 0, 0, '', 0, 0, 0, '', 0, 0, '2023-04-26 18:30:00'),
+('asdfghj', '', 'sdfghj', 'sdzfghjf', '', ' web devloper resume saikishor 1 (2).pdf', 0, 0, '', 0, 0, 0, '', 0, 0, '2023-04-26 18:30:00'),
+('cvbhjnkm', '', '', '', '', ' ', 0, 0, '', 0, 0, 0, '', 0, 0, '2023-04-26 18:30:00'),
+('awesgdfbc', '', '', '', '', ' ', 0, 0, '', 0, 0, 0, '', 0, 0, '2023-04-26 18:30:00'),
+('aesgdfhfg', '', '', '', '', ' ', 0, 0, '', 0, 0, 0, '', 0, 0, '2023-04-26 18:30:00');
+
+-- --------------------------------------------------------
+
 --
--- Indexes for table `form1`
+-- Table structure for table `pofoma3`
 --
-ALTER TABLE `form1`
-  ADD PRIMARY KEY (`form1ID`);
+
+CREATE TABLE `pofoma3` (
+  `computerlab` text NOT NULL,
+  `computerdetailsname` text NOT NULL,
+  `intenet` text NOT NULL,
+  `connectivity` text NOT NULL,
+  `electricity` text NOT NULL,
+  `upsbattery` text NOT NULL,
+  `upscapacity` text NOT NULL,
+  `compteachtable` text NOT NULL,
+  `copyoftimetable` blob NOT NULL,
+  `yesno` enum('yes','no') NOT NULL,
+  `compcourse` enum('mscit','ccc') NOT NULL,
+  `exampass` enum('dca','bca','bsc') NOT NULL,
+  `equivalentto` text NOT NULL,
+  `teacherisnotavailable` text NOT NULL,
+  `hallforcomputerlab` text NOT NULL,
+  `sizeofhall` text NOT NULL,
+  `hallsecured` text NOT NULL,
+  `date` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
