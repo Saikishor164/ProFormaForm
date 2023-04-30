872415c99a0d869d9937244f1685267e4009e455
Saikishor Rasala <rasalasaikishor@gmail.com>
2023-04-30 12:53:38 +0530

    this fixes #23

--- form2.php
+++ form2.php
@@ -23,27 +23,7 @@
 
     
 
-    // // Validate form data
-    // $errors = array();
-    // if(empty($name)) {
-    //     //$errors["name"] = "Please enter your name";
-    // }
-    // if(empty($email)) {
-    //     //$errors["email"] = "Please enter your email address";
-    // } elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    //     //$errors["email"] = "Invalid email address";
-    // }
-    // if(empty($password)) {
-    //     //$errors["password"] = "Please enter a password";
-    // } elseif(strlen($password) < 8) {
-    //     //$errors["password"] = "Password must be at least 8 characters long";
-    // }
-    // if(empty($confirm_password)) {
-    //     //$errors["confirm_password"] = "Please confirm your password";
-    // } elseif($password != $confirm_password) {
-    //     //$errors["confirm_password"] = "Passwords do not match";
-    // }
-
+    
     // If there are no errors, insert user data into database
     if(empty($errors)) {
         // Connect to database
