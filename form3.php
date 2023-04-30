861901573b0596a1929c17a17df88b47c348c6b6
Saikishor Rasala <rasalasaikishor@gmail.com>
2023-04-30 12:54:11 +0530

    this fixes #24

--- form3.php
+++ form3.php
@@ -1,32 +1,31 @@
 <?php
 
 
-
-if($_SERVER["REQUEST_METHOD"] == "POST") {
     // Get form data
-   
-    // Validate form data
-    $errors = array();
-    if(empty($name)) {
-        //$errors["name"] = "Please enter your name";
-    }
-    if(empty($email)) {
-        //$errors["email"] = "Please enter your email address";
-    } elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        //$errors["email"] = "Invalid email address";
-    }
-    if(empty($password)) {
-        //$errors["password"] = "Please enter a password";
-    } elseif(strlen($password) < 8) {
-        //$errors["password"] = "Password must be at least 8 characters long";
-    }
-    if(empty($confirm_password)) {
-        //$errors["confirm_password"] = "Please confirm your password";
-    } elseif($password != $confirm_password) {
-        //$errors["confirm_password"] = "Passwords do not match";
-    }
+    $computerlab = $_POST["computerlab"];
+    $computerdetailsname = $_POST["computerdetailsname"];
+    $intenet = $_POST["intenet"];
+    $connectivity = $_POST["connectivity"];
+    $electricity = $_POST["electricity"];
+    $upsbattery = $_POST["upsbattery"];
+    $upscapacity = $_POST["upscapacity"];
+    $compteachtable = $_POST["compteachtable"];
+    $copyoftimetable = $_POST["copyoftimetable"];
+    $yesno = $_POST["yesno"];
+    $compcourse = $_POST["compcourse"];
+    $exampass = $_POST["exampass"];
+    $equivalentto = $_POST["equivalentto"];
+    $teacherisnotavailable = $_POST["teacherisnotavailable"];
+    $hallforcomputerlab = $_POST["hallforcomputerlab"];
+    $sizeofhall = $_POST["sizeofhall"];
+    $hallsecured = $_POST["hallsecured"];
+    $date = $_POST["date"];
+    
+   // echo "$schooljr.' - '.$school' - '.$Instructionname' - '.$primaryname' - '.$adid' - '.$strength' - '.$teachingstaff' - '.$secondaryschool' - '.$adid2' - '.$secstrength' - '.$secteachingstaffsec' - '.$jrschool' - '.$adid3' - '.$jrstrength' - '.$jrteachingstaff' - '.$date" ;
 
-    // If there are no errors, insert user data into database
+    
+
+    
     if(empty($errors)) {
         // Connect to database
         $servername = "localhost";
@@ -41,11 +40,12 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
         }
 
         // Insert data into database
-        $sql = "INSERT INTO pofoma2 ()
-                VALUES ()";
+        $sql = " INSERT INTO pofoma3 (`computerlab`, `computerdetailsname`, `intenet`, `connectivity`, `electricity`, `upsbattery`, `upscapacity`, `compteachtable`, `copyoftimetable`, `yesno`, `compcourse`, `exampass`, `equivalentto`, `teacherisnotavailable`, `hallforcomputerlab`, `sizeofhall`, `hallsecured`, `date`) 
+                VALUES ('$computerlab',' $computerdetailsname', '$intenet', '$connectivity', '$electricity', '$upsbattery',' $upscapacity', '$compteachtable',' $copyoftimetable',' $yesno',' $compcourse',' $exampass', '$equivalentto',' $teacherisnotavailable',' $hallforcomputerlab',' $sizeofhall', '$hallsecured',' $date')";
 
         if ($conn->query($sql) === TRUE) {
-            echo "Registration successful";
+            echo " thank you"; 
+            
         } else {
             echo "Error: " . $sql . "<br>" . $conn->error;
         }
@@ -57,6 +57,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
             echo "<p>$error</p>";
         }
     }
-}
+
 
 ?>
